67bccf8a32d13fd17cbdb347a52047e088fbb938
matheus <matheuspsp24@gmail.com>
2025-12-10 14:03:35 -0300

    atualizaçao

--- src/models/10Piadas.js
+++ src/models/10Piadas.js
@@ -2,7 +2,7 @@ import { openDb } from '../config/database.js';
 
 export async function getPiadaAleatoria10() {
     const db = await openDb();
-    const piada = await db.get(
+    const piada = await db.all(
        `  SELECT id, pergunta, resposta
             FROM piadas WHERE aprovada = 1 ORDER BY RANDOM() LIMIT 10
        ` 
